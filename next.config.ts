c2570a947ec2d3ad756c7489c08dc70a6d5d32d0
Nikola2112 <nikola21122004@gmail.com>
2025-06-08 11:18:29 +0100

    all start

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-import { i18n } from './next-i18next.config.js';
-
-export default {
-    i18n,
-    reactStrictMode: true,
-};
--- src/app/components/Contacts.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-export default function Contacts() {
-    return (
-        <section id="contacts" className="p-8">
-            <h2 className="text-3xl text-neon mb-4">Контакти</h2>
-            <p>Тут будуть мої контакти.</p>
-        </section>
-    );
-}
